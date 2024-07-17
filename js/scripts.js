f4f70ff9dfc1de748773dd35b82ee2e35b35a794
Achmad Yama Roni S <yamaroni8@gmail.com>
2024-07-17 20:02:16 +0700

    update js css & html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // Ini javascript
+
+function updateResult() {
+    let weightValue = document.querySelector('#input-berat-badan').value;
+    console.log(weightValue);
+    // jika weightValue tidak sama dengan null dan wajib weightValue tidak sama dengan ''
+    weightValue != null && weightValue != ''
+        ? document.querySelector('#bmi-result').innerHTML = weightValue
+        : alert('inputan anda kosong!');
+}
